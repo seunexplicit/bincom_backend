a22f5eb8931a521f91e62bf0f56e0f4016eaf95d
seunexplicit <seunxplicit@gmail.com>
2021-10-13 18:22:57 +0100

    .

--- bincom_test.sql
+++ bincom_test.sql
@@ -23,7 +23,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 -- --------------------------------------------------------
 
-USE bincomphptest;
+-- USE bincomphptest;
 
 --
 -- Table structure for table `agentname`
